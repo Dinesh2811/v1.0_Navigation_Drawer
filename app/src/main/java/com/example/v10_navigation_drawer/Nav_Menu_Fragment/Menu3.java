c29f45ad5d89cde8cee7a599fa61d55ed903dcf3
Dinesh <dineshk281198@gmail.com>
2022-03-28 09:33:01 +0530

    v1.0

---
+++ app/src/main/java/com/example/v10_navigation_drawer/Nav_Menu_Fragment/Menu3.java
@@ -0,0 +1,21 @@
+package com.example.v10_navigation_drawer.Nav_Menu_Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.v10_navigation_drawer.R;
+
+public class Menu3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_menu3,container,false);
+    }
+}
